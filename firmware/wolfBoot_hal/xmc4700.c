0263b3a6a97f8a9026697ccc2c3d4ea1b815ee74
mthielvoldt <mthielvoldt@gmail.com>
2025-02-13 17:39:43 -0500

    Implement xmc4700 wolfBoot HAL

--- firmware/wolfBoot_hal/xmc4700.c
+++ firmware/wolfBoot_hal/xmc4700.c
@@ -3,95 +3,45 @@
  */
 
 #include <stdint.h>
+#include <xmc4_flash.h>
 
-#include "image.h"
+#include <image.h>
 
 
 #ifndef ARCH_FLASH_OFFSET
-// Cached mirror. Table 7-2
-#define ARCH_FLASH_OFFSET 0x08000000U
+// Access address that bypasses the prefetch cache. Table 7-2
+#define ARCH_FLASH_OFFSET XMC_FLASH_UNCACHED_BASE
 #endif
 
 /* Assembly helpers - Data Memory Barrier */
-#define DMB() asm volatile ("dmb")
-
+// #define DMB() asm volatile ("dmb")
 
 /*** FLASH ***/
-#define APB1_CLOCK_ER           (*(volatile uint32_t *)(0x40023840U))
-#define APB1_CLOCK_RST          (*(volatile uint32_t *)(0x40023820U))
-#define TIM2_APB1_CLOCK_ER_VAL     (1 << 0)
-#define PWR_APB1_CLOCK_ER_VAL   (1 << 28)
+#define PAGE_SIZE_BYTES     256U
 
-#define FLASH_BASE          (0x40023C00U)
-#define FLASH_ACR           (*(volatile uint32_t *)(FLASH_BASE + 0x00U))
-#define FLASH_KEYR          (*(volatile uint32_t *)(FLASH_BASE + 0x04U)) // unlock
-#define FLASH_SR            (*(volatile uint32_t *)(FLASH_BASE + 0x0CU)) //
-#define FLASH_CR            (*(volatile uint32_t *)(FLASH_BASE + 0x10U)) // all flash ops
+#define FLASH_TOP      (uint32_t *)(XMC_FLASH_UNCACHED_BASE + 0x0200000UL)
 
-/* Register values */
-#define FLASH_ACR_RESET_DATA_CACHE            (1 << 12)
-#define FLASH_ACR_RESET_INST_CACHE            (1 << 11)
-#define FLASH_ACR_ENABLE_DATA_CACHE           (1 << 10)
-#define FLASH_ACR_ENABLE_INST_CACHE           (1 << 9)
-#define FLASH_ACR_ENABLE_PRFT                 (1 << 8)
+#define FLASH_SECTOR_COUNT 16
+const uint32_t sector_base[FLASH_SECTOR_COUNT + 1] = {
+    XMC_FLASH_SECTOR_0,  /* 16 Kb   */
+    XMC_FLASH_SECTOR_1,  /* 16 Kb   */
+    XMC_FLASH_SECTOR_2,  /* 16 Kb   */
+    XMC_FLASH_SECTOR_3,  /* 16 Kb   */
 
-#define FLASH_SR_BSY                          (1 << 16) // to wait for complete.
-#define FLASH_SR_PGSERR                       (1 << 7) // used to clear errors. 
-#define FLASH_SR_PGPERR                       (1 << 6)
-#define FLASH_SR_PGAERR                       (1 << 5)
-#define FLASH_SR_WRPERR                       (1 << 4)
-#define FLASH_SR_OPERR                        (1 << 1)
-#define FLASH_SR_EOP                          (1 << 0)
+    XMC_FLASH_SECTOR_4,  /* 16 Kb   */
+    XMC_FLASH_SECTOR_5,  /* 16 Kb   */
+    XMC_FLASH_SECTOR_6,  /* 16 Kb   */
+    XMC_FLASH_SECTOR_7,  /* 16 Kb   */
 
-#define FLASH_CR_LOCK                         (1UL << 31) // unlock
-#define FLASH_CR_ERRIE                        (1 << 25)
-#define FLASH_CR_EOPIE                        (1 << 24)
-#define FLASH_CR_STRT                         (1 << 16)
-#define FLASH_CR_MER                          (1 << 2)
-#define FLASH_CR_SER                          (1 << 1)
-#define FLASH_CR_PG                           (1 << 0) // 
+    XMC_FLASH_SECTOR_8,  /* 128 Kb  */
+    XMC_FLASH_SECTOR_9,  /* 256 Kb  */
+    XMC_FLASH_SECTOR_10, /* 256 Kb  */
+    XMC_FLASH_SECTOR_11, /* 256 Kb  */
 
-#define FLASH_CR_SNB_SHIFT                      3
-#define FLASH_CR_SNB_MASK                      0x1f
-
-#define FLASH_CR_PROGRAM_X8                   (0 << 8)
-#define FLASH_CR_PROGRAM_X16                  (1 << 8)
-#define FLASH_CR_PROGRAM_X32                  (2 << 8)
-#define FLASH_CR_PROGRAM_X64                  (3 << 8)
-
-#define FLASH_KEY1                            (0x45670123U) // unlock
-#define FLASH_KEY2                            (0xCDEF89ABU) // unlock
-
-
-/* FLASH Geometry */
-#define FLASH_SECTOR_0  0x0000000 /* 16 Kb   */
-#define FLASH_SECTOR_1  0x0004000 /* 16 Kb   */
-#define FLASH_SECTOR_2  0x0008000 /* 16 Kb   */
-#define FLASH_SECTOR_3  0x000C000 /* 16 Kb   */
-#define FLASH_SECTOR_4  0x0010000 /* 64 Kb   */
-#define FLASH_SECTOR_5  0x0020000 /* 128 Kb  */
-#define FLASH_SECTOR_6  0x0040000 /* 128 Kb  */
-#define FLASH_SECTOR_7  0x0060000 /* 128 Kb  */
-#define FLASH_SECTOR_8  0x0080000 /* 128 Kb  */
-#define FLASH_SECTOR_9  0x00A0000 /* 128 Kb  */
-#define FLASH_SECTOR_10 0x00C0000 /* 128 Kb  */
-#define FLASH_SECTOR_11 0x00E0000 /* 128 Kb  */
-#define FLASH_TOP       0x0100000
-
-#define FLASH_SECTORS 12
-const uint32_t flash_sector[FLASH_SECTORS + 1] = {
-    FLASH_SECTOR_0,
-    FLASH_SECTOR_1,
-    FLASH_SECTOR_2,
-    FLASH_SECTOR_3,
-    FLASH_SECTOR_4,
-    FLASH_SECTOR_5,
-    FLASH_SECTOR_6,
-    FLASH_SECTOR_7,
-    FLASH_SECTOR_8,
-    FLASH_SECTOR_9,
-    FLASH_SECTOR_10,
-    FLASH_SECTOR_11,
+    XMC_FLASH_SECTOR_12, /* 256 Kb  */
+    XMC_FLASH_SECTOR_13, /* 256 Kb  */
+    XMC_FLASH_SECTOR_14, /* 256 Kb  */
+    XMC_FLASH_SECTOR_15, /* 256 Kb  */
     FLASH_TOP
 };
 
@@ -107,60 +57,113 @@ void hal_prepare_boot(void)
 
 void RAMFUNCTION hal_flash_unlock(void)
 {
-    FLASH_CR |= FLASH_CR_LOCK;
-    FLASH_KEYR = FLASH_KEY1;
-    FLASH_KEYR = FLASH_KEY2;
+  // There is no global flash unlock on XMC.
+  // Each page write and sector erase operation requires a command sequence.
+
+  // If write protection is enabled, it is temporarily disabled sector-by-
+  // sector with a pre-stored user password.
 }
 
-void RAMFUNCTION hal_flash_lock(void)
+void RAMFUNCTION hal_flash_lock(void) { }
+
+/** hal_flash_write
+ * @param address Either an offset from the program memory base address, or an
+ * absolute address.  Un-aligned addresses are permitted.
+ * @param data the payload to be stored in the flash.
+ * @param len the size of the payload. Valid range: [1, WOLFBOOT_SECTOR_SIZE]
+ * @returns 0 on success, a negative value on failure.
+ * The XMC flash memory model only supports writing aligned 256-byte pages at a
+ * time.  So this function implements a read-modify-write process than handles
+ * both un-aligned start and end addresses.
+ */
+int RAMFUNCTION hal_flash_write(uint32_t address, const uint8_t *data, int len)
 {
-    FLASH_CR |= FLASH_CR_LOCK;
+  uint8_t page_buffer[PAGE_SIZE_BYTES];
+  int bytesWritten = 0;
+  
+  
+  /* adjust for flash base to allow for both offsets and absolute addresses. */
+  if (address < ARCH_FLASH_OFFSET) {
+    address += ARCH_FLASH_OFFSET;
+  }
+  
+  /* Find the closest page-aligned address preceeding first address to write*/
+  uint32_t page_adr = getPreceedingPageBoundary(address);
+  int last_address_to_write = address + len;
+  uint32_t write_end_adr, write_start_adr, bytes_written = 0;
+
+  while (last_address_to_write > page_adr) {
+    memset(page_buffer, 0, PAGE_SIZE_BYTES);
+    write_start_adr = address + bytes_written;
+    write_end_adr = (page_adr + PAGE_SIZE_BYTES < last_address_to_write) ? 
+      page_adr + PAGE_SIZE_BYTES : last_address_to_write;
+    
+    //  WRITE FIRST PAGE
+    //      _______buffer______ 
+    //     |.......+===========|=======+............|
+    //     ^       ^           ^
+    //  page_adr  wr_start   wr_end
+    //
+    //  WRITE SECOND PAGE
+    //                          _______buffer_______
+    //     |.......+===========|=======+............|
+    //                         ^       ^
+    //                     page_adr  wr_end
+    //                     wr_start
+    memcpy(page_buffer + (write_start_adr - page_adr),
+           data + bytes_written,
+           write_end_adr - write_start_adr
+    );
+    XMC_FLASH_ProgramPage((uint32_t*)page_adr, page_buffer);
+
+    // Prepare for next page.
+    bytes_written += write_end_adr - write_start_adr;
+    page_adr += PAGE_SIZE_BYTES;
+  }
+  return 0;
 }
 
-int RAMFUNCTION hal_flash_write(uint32_t _address, const uint8_t *data, int len)
+/** hal_flash_erase
+ * Erase all the sectors within the specified range, accounting for flash sector
+ * geometries.
+ * @param start_address marks the start of the area to erase. start_address is
+ * guaranteed to be aligned to WOLFBOOT_SECTOR_SIZE.
+ * @param len specifies the size of the area to be erased. Len is guaranteed to 
+ * be a multiple of WOLFBOOT_SECTOR_SIZE. 
+ */
+int RAMFUNCTION hal_flash_erase(uint32_t start_address, int len)
 {
-    int i;
-    uint32_t address = _address - ARCH_FLASH_OFFSET;
-    flash_wait_complete();
-    clear_errors();
-    /* Set 8-bit write */
-    FLASH_CR &= (~(0x03 << 8));
-    for (i = 0; i < len; i++) {
-        FLASH_CR |= FLASH_CR_PG;
-        *(uint8_t*)(address + (uint32_t)i) = data[i];
-        flash_wait_complete();
-        FLASH_CR &= ~FLASH_CR_PG;
+  // Work with absolute addresses. 
+  if (start_address < ARCH_FLASH_OFFSET) {
+    start_address += ARCH_FLASH_OFFSET;
+  }
+  if (len == 0) {
+    return -1;
+  }
+  
+  uint32_t end_address = start_address + len - 1; 
+
+  int start_sector = getSectorContainingAddress(start_address);
+  int end_sector = getSectorContainingAddress(end_address);
+  if (start_sector < 0 || end_sector < 0){
+    return -1;
+  }
+
+  for (int sector = start_sector; sector <= end_sector; sector++)
+    XMC_FLASH_EraseSector(sector_base[sector])
+  return 0;
+}
+
+/**
+ * @param address must be an absolute address (not an offset).
+ */
+static int getSectorContainingAddress(uint32_t address) {
+  int ret = -1;
+  for (int sectorIdx = 0; sectorIdx < FLASH_SECTOR_COUNT; sectorIdx++) {
+    if ((address >= sector_base[sectorIdx]) && \
+        (address <  sector_base[sectorIdx + 1])) {
+          ret = sectorIdx;
     }
-    return 0;
-}
-
-int RAMFUNCTION hal_flash_erase(uint32_t _address, int len)
-{
-    int start = -1, end = -1;
-    uint32_t end_address;
-    int i;
-    uint32_t address = _address - ARCH_FLASH_OFFSET;
-    if (len == 0)
-        return -1;
-
-    end_address = address + len - 1;
-
-    if (address < flash_sector[0] || end_address > FLASH_TOP)
-        return -1;
-    for (i = 0; i < FLASH_SECTORS; i++)
-    {
-        if ((address >= flash_sector[i]) && (address < flash_sector[i + 1])) {
-            start = i;
-        }
-        if ((end_address >= flash_sector[i]) && (end_address < flash_sector[i + 1])) {
-            end = i;
-        }
-        if (start > 0 && end > 0)
-            break;
-    }
-    if (start < 0 || end < 0)
-        return -1;
-    for (i = start; i <= end; i++)
-        flash_erase_sector(i);
-    return 0;
+  }
+  return ret;
 }
