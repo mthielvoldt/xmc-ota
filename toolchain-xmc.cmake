99cfae5fa6b0de314c054aae14522a1bb5de7fe1
mthielvoldt <mthielvoldt@gmail.com>
2025-02-20 18:28:47 -0500

    BL and app both building

--- toolchain-xmc.cmake
+++ toolchain-xmc.cmake
@@ -2,7 +2,6 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
 set(CMAKE_C_STANDARD 11)
-set(CMAKE_C_EXTENSIONS OFF)
 
 # Suppresses the check for a working compiler, which will fail for sys calls since there's no sys.
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
