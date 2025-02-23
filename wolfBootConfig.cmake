fd88ce02ef127f5cc4ce9a3aa7ef43381ab32fce
mthielvoldt <mthielvoldt@gmail.com>
2025-02-23 10:12:19 -0500

    Set IMAGE_HEADER_SIZE for XMC4 requirements

--- wolfBootConfig.cmake
+++ wolfBootConfig.cmake
@@ -13,6 +13,10 @@ set(CACHED_PARTITION_BOOT_ADDRESS     0x08040000)
 set(WOLFBOOT_PARTITION_UPDATE_ADDRESS 0x0C080000) # sector 10
 set(WOLFBOOT_PARTITION_SWAP_ADDRESS   0x0C0C0000) # sector 11
 
+# XMC4 requires the VTOR be 1024-byte aligned. (256 4-byte words)
+# see Vector Table Offset Register 2-64 (p123)
+set(IMAGE_HEADER_SIZE 1024)
+
 set(USER_HAL_SOURCES
   ${XMCLIB}/src/xmc4_flash.c
 )
